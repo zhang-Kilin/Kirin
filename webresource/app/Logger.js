d40505641d79cdfb7b81aca24cce6203e1781551
zhanghh <zhanghh@Ctrip.com>
2016-05-03 13:42:18 +0800

    logger 放弃监听window.onerror

--- webresource/app/Logger.js
+++ webresource/app/Logger.js
@@ -2,10 +2,10 @@ define('Logger',[],function(){
 
 	var Logger = function(){
 		//监听并接管error事件
-		window.onerror = $.proxy(function(){
-			this.error.apply(this,arguments);
-			return true;
-		},this);
+		// window.onerror = $.proxy(function(){
+		// 	this.error.apply(this,arguments);
+		// 	return true;
+		// },this);
 	};
 
 	_.extend(Logger.prototype,{
